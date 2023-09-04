5a043442a4639c002823c2d6273ebf4fd6036d87
Ben Jackson <benjacksonsmac@gmail.com>
2023-09-04 13:19:58 +1200

    ben's auth

--- spotify-clone-client/src/App.jsx
+++ spotify-clone-client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import BenProfile from './components/Ben'
+
 
 function App() {
 
   return (
     <>
       <div>
-       
+        <h1>Hello</h1>
+       <BenProfile />
       </div>
       
     </>
