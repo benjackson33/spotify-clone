3258bf15761e16332c5104db380d02833139d61f
Gareth Lee <lkl.g@hotmail.com>
2023-09-11 17:54:07 +1000

    Working on RefreshToken

--- server/server.js
+++ server/server.js
@@ -69,17 +69,18 @@ app.get("/callback", async (req, res) => {
       if (response.status === 200) {
         const { data } = response
 
-        try {
-          const profileResponse = await axios.get('https://api.spotify.com/v1/me', {
-            headers: {
-              Authorization: `Bearer ${data.access_token}`
-            }   
-          })
-          res.send(profileResponse.data)
-        } catch (err) {
-          res.send(err)
-        }
+        res.send(data)
 
+        // try {
+        //   const profileResponse = await axios.get('https://api.spotify.com/v1/me', {
+        //     headers: {
+        //       Authorization: `Bearer ${data.access_token}`
+        //     }   
+        //   })
+        //   res.send(profileResponse.data)
+        // } catch (err) {
+        //   res.send(err)
+        // }
       } else {
         res.send(response);
       }
@@ -93,34 +94,50 @@ app.get("/callback", async (req, res) => {
 app.get("/refresh_token", async (req, res) => {
   const { refresh_token } = req.query
 
-  let data = {
+  let args = new URLSearchParams ({
     grant_type: "refresh_token",
     refresh_token: refresh_token
-  }
+  })
 
-  try {
-    const response = await axios({
+  await axios({
       method: "POST",
       url: 'https://accounts.spotify.com/api/token',
-      data: data,
+      data: args,
       headers: {
         'Authorization': `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
         "Content-Type": "application/x-www-form-urlencoded"
       }
+  })
+    .then(response => {
+      res.send(response)
+    })
+    .catch(err => {
+      res.send(err)
     })
 
-    if (response.status === 200) {
-      const { access_token } = req.query.data
-      // const refresh_response = await axios.get(`http://localhost:3001/refresh_token?refresh_token=${refresh_token}`)
-      res.send({
-        'access_token': access_token
-      })
-    } else {
-      res.send(response)
-    }
-  } catch (err) {
-    res.send(err)
-  }
+  // try {
+  //   const response = await axios({
+  //     method: "POST",
+  //     url: 'https://accounts.spotify.com/api/token',
+  //     data: args,
+  //     headers: {
+  //       'Authorization': `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
+  //       "Content-Type": "application/x-www-form-urlencoded"
+  //     }
+  //   })
+
+  //   if (response.status === 200) {
+  //     const access_token = response.access_token
+  //     // const refresh_response = await axios.get(`http://localhost:3001/refresh_token?refresh_token=${refresh_token}`)
+  //     res.send({
+  //       'access_token': access_token
+  //     })
+  //   } else {
+  //     res.send(response)
+  //   }
+  // } catch (err) {
+  //   res.send(err)
+  // }
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
