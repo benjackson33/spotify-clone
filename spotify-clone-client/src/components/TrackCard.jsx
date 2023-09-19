88ccd573d51e6b723f81d09308285c34bc0d9df9
Gareth Lee <lkl.g@hotmail.com>
2023-09-20 09:09:34 +1000

    TrackCard edited. CSS Module added.

--- spotify-clone-client/src/components/TrackCard.jsx
+++ spotify-clone-client/src/components/TrackCard.jsx
@@ -3,7 +3,7 @@ import styles from "./TrackCard.module.css"
 
 const TrackCard = ({ track }) => {
     return (
-        <li className="card">
+        <li className={styles.card}>
             <img src={track.album.images[0].url} alt="" />
             <h3>Name: {track.name}</h3>
             <p>Duration: {timeConversion(track.duration_ms)}</p>
