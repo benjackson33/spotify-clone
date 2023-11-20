ca9120e276c97b848b3374b1cac2a0181cafe11e
Ben Jackson <benjacksonsmac@gmail.com>
2023-11-21 12:36:19 +1300

    playlist header

--- spotify-clone-client/src/components/NavBar.jsx
+++ spotify-clone-client/src/components/NavBar.jsx
@@ -3,18 +3,22 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   let navList = [
     {
+      id: 1,
       text: "Home",
       link: "/",
     },
     {
+      id: 2,
       text: "Profile",
       link: "/profile",
     },
     {
+      id: 3,
       text: "Top Tracks",
       link: "/top-tracks",
     },
     {
+      id: 4,
       text: "Playlists",
       link: "/playlists"
     }
@@ -23,9 +27,9 @@ const NavBar = () => {
   return (
     <nav className="sidebar" >
       <ul>
-        {navList.map(({ text, link }) => (
-          <li>
-            <NavLink to={link}>{text}</NavLink>
+        {navList.map(({ id, text, link }) => (
+          <li key={id}>
+            <NavLink  to={link}>{text}</NavLink>
           </li>
         ))}
       </ul>
--- spotify-clone-client/src/components/PlaylistHeader.jsx
+++ spotify-clone-client/src/components/PlaylistHeader.jsx
@@ -1,9 +1,34 @@
-const PlaylistHeader = ({ playlist }) => {
-    return (
-        <>
-        <img src="" alt="" />
-        </>
-    )
-}
+import styles from "../styles/PlaylistHeader.module.css";
 
-export default PlaylistHeader
+const PlaylistHeader = ({ playlist }) => {
+  if (
+    !playlist ||
+    !playlist.owner ||
+    !playlist.name ||
+    !playlist.images ||
+    !playlist.images.length
+  ) {
+    return null; // or render a placeholder/error message
+  }
+
+  const { name, images, owner } = playlist;
+  const playlistName = name;
+  const playlistImage = images[0].url;
+  const playlistOwner = owner.display_name;
+
+  // console.log(owner);
+
+  return (
+    <>
+      <div className={styles.headerWrapper}>
+        <img className={styles.headerImg} src={playlistImage} alt="" />
+        <h1>{playlistName}</h1>
+        <div className={styles.playlistTitle}>
+          <p>{playlistOwner}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlaylistHeader;
